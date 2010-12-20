fee9e56b837cb63d20d74ade2ea1d58b4407621e
gianluigi.avella@gmail.com <gianluigi.avella@gmail.com@af0d00ee-63ef-a7dc-0187-bacee1410c48>
2010-12-20 14:37:59 +0000

    La classe Messaggio è una classe di utilità che a seconda dell'esito restituisce il giusto messaggio all'operatore

---
+++ UNICARS/src/unicars/utility/Messaggio.java
@@ -0,0 +1,18 @@
+package unicars.utility;
+
+public class Messaggio {
+	
+	private String messaggio;
+
+	public Messaggio(String messaggio) {
+		this.messaggio = messaggio;
+	}
+
+	public String getMessaggio() {
+		return messaggio;
+	}
+
+	public void setMessaggio(String messaggio) {
+		this.messaggio = messaggio;
+	}
+}
